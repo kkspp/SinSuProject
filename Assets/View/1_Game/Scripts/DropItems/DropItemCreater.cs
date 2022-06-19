2d23dc475aa96fe393dbd06e8ad004040e6e9e6d
KyoungSu Kim <kkspp@naver.com>
2022-06-19 17:02:43 +0900

    drop item 생성 위치 수정

--- Assets/View/1_Game/Scripts/DropItems/DropItemCreater.cs
+++ Assets/View/1_Game/Scripts/DropItems/DropItemCreater.cs
@@ -7,22 +7,19 @@ public class DropItemCreater : MonoBehaviour
     [SerializeField]
     private List<GameObject> dropItemPrefabs;
 
-    public bool isActivatied;
-
     private void Start()
     {
-        isActivatied = false;
         StartCoroutine("CreateRandomItems");
     }
 
     IEnumerator CreateRandomItems()
     {
-        while(!isActivatied)
+        while(true)
         {
             int selectedItemIndex = Random.Range(0, 6);
             int randomGravityScale = Random.Range(0, 6) * 15;
-            int randomTime = Random.Range(0, 3);
-            float randomDropItemPosX = Random.Range(-Screen.width, Screen.width) / 2;
+            float randomTime = Random.Range(0, 1.5f);
+            float randomDropItemPosX = Random.Range(0, Screen.width);
             DropItem dropItem = Instantiate(dropItemPrefabs[selectedItemIndex], new Vector2(randomDropItemPosX, Screen.height+150), Quaternion.Euler(Vector3.zero), transform).GetComponent<DropItem>();
             dropItem.GravityScale = randomGravityScale;
             yield return new WaitForSeconds(randomTime);
