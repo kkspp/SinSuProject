e4c0d63f82d2ee344d4019a01e0c6f35d1944a6d
KyoungSu Kim <kkspp@naver.com>
2022-06-25 21:33:28 +0900

    feat: add joystick

--- Assets/View/1_Game/Scripts/CharacterInputModule.cs
+++ Assets/View/1_Game/Scripts/CharacterInputModule.cs
@@ -4,28 +4,33 @@ using UnityEngine;
 
 public class CharacterInputModule : MonoBehaviour
 {
-    const int CharacterWidth = 180; 
+    const int CharacterWidth = 180;
+    [SerializeField]
+    private Joystick joystic;
     [SerializeField]
     private float moveSpeed;
 
     private RectTransform characterRectTransform;
+    private Rigidbody2D userRigidbody;
 
     private void Start()
     {
         characterRectTransform = GetComponent<RectTransform>();
+        userRigidbody = GetComponent<Rigidbody2D>();
 
         Debug.Log(CharacterWidth / 2.0f);
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
+        Vector2 direction = Vector2.right * joystic.Horizontal;
+        userRigidbody.AddForce(direction * moveSpeed * Time.fixedDeltaTime, ForceMode2D.Impulse);
+
         if (characterRectTransform.anchoredPosition.x <= (-Screen.width + CharacterWidth) / 2.0f)
             characterRectTransform.anchoredPosition = new Vector2((-Screen.width + CharacterWidth) / 2.0f, characterRectTransform.anchoredPosition.y);
         else if (characterRectTransform.anchoredPosition.x >= (Screen.width - CharacterWidth) / 2.0f)
             characterRectTransform.anchoredPosition = new Vector2((Screen.width - CharacterWidth) / 2.0f, characterRectTransform.anchoredPosition.y);
 
-        float horizontalKey = Input.GetAxis("Horizontal");
-        GetComponent<RectTransform>().anchoredPosition += new Vector2(horizontalKey * moveSpeed, 0);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
